817c1e61a3826cce4a747c307f6aec701cfe70b2
jjm8759 <89434744+jjm8759@users.noreply.github.com>
2022-09-22 11:36:45 -0400

    Semantics

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 import app from './server.js';
 import mongodb from 'mongodb';
 import dotenv from 'dotenv';
-
 async function main() {
+
     dotenv.config();
 
     const client = new mongodb.MongoClient(
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import users from './api/users.route.js'
+import express from 'express';
+import cors from 'cors';
+import users from './api/users.route.js';
+import exphbs from 'express-handlebars';
+import morgan from 'morgan';
 
-const morgan = require("morgan")
-const app = express()
+
+const app = express();
 
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
-}
-app.use(cors())
-app.use(express.json)
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json);
+app.use(exphbs);
+app.use("/api/v1/users", users);
 
-app.use("/api/v1/users", users)
 app.use('*', (req,res) => {
     res.status(404).json({error: "not found"})
 })
