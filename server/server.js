57041dc6f4faffbe1ef75fa3a4eb9c4913ad8ea0
jjm8759 <89434744+jjm8759@users.noreply.github.com>
2022-09-22 11:20:00 -0400

    Added morgan to server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import users from './api/users.route.js'
 
+const morgan = require("morgan")
 const app = express()
 
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
 app.use(cors())
 app.use(express.json)
 
