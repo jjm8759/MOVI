86eb20645cb0609a3b719bfb09a5cf6829ef741f
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2022-10-05 01:28:00 -0400

    Added sample data creation code to index.js

--- movi/server/index.js
+++ movi/server/index.js
@@ -7,6 +7,12 @@ import dotenv from 'dotenv';
 
 import app from './app.js'; // Import the express app from app.js
 
+// import models for testing
+import User from './models/user.js';
+import Title from './models/Title.js';
+import WatchedTitle from './models/watchedTitle.js';
+import RecommendedTitle from './models/recommendedTitle.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -21,3 +27,37 @@ mongoose.connect(process.env.MOVI_DB_URI, {
         });
     })
     .catch((error) => console.log(error.message)); // If error, catch it...
+
+// Create a new User and insert into database
+const user = await User.create({
+    firstName: "Will",
+    lastName: "Robinson",
+    email: "robots@beep.boop",
+    password: "iheartrobots",
+    watchModes: ["NETFLIX", "DISNEYPLUS"],
+});
+
+const title = await Title.create({
+    watchmodeId: 1234,
+    title: "That Movie 2",
+});
+
+const watchedMovie = await WatchedTitle.create({
+    user: user,
+    title: title,
+    favorited: false,
+    userStars: 5
+});
+
+const recommendedTitle = await RecommendedTitle.create({
+    user: user,
+    title: title,
+    watchlinkClicked: true,
+    clickActionDate: Date.now()
+});
+
+user.watchedTitles.push(watchedMovie);
+user.recommendedTitles.push(recommendedTitle);
+await user.save();
+  
+console.log(user);
