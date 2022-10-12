8823011b7a23f69647340c2b852f27e4ecb7fcda
Domenic Mangano <domenicmangano@domenics-mbp.wireless.rit.edu>
2022-10-12 17:03:56 -0400

    committing Discover React

---
+++ movi/client/pages/DiscoverPage/components/Containers.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import {Component} from 'react';
+
+
+
+export default function Container() {
+    return (
+    <>
+      <Container maxWidth="lg">
+        <h1  style={{ backgroundColor: '#cfe8fc'}}>
+          What's Hot
+         </h1>
+      </Container>
+    </>
+    );
+}
