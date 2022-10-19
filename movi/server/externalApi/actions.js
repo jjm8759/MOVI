8126209a213e8ef252be6421cf97c32e9496c9d0
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2022-10-18 23:16:58 -0400

    Added docstring for getListTitlesResults function

--- movi/server/externalApi/actions.js
+++ movi/server/externalApi/actions.js
@@ -20,7 +20,7 @@ export const populateTitleById = async (id) => {
 }
 
 /**
- * This function queries WatchMode for titles whose names contain
+ * This function queries external API(s) for titles whose names contain
  * the provided substring.
  * @param {String} search_query The string query to search with
  * @returns a list of search result objects
@@ -29,8 +29,17 @@ export const populateTitleById = async (id) => {
     return await watchmode.fetchTitlesByPartialString(search_query);
 }
 
-export const getListTitlesResults = async (types, source_types, source_ids, genre_ids) => {
-    return await watchmode.fetchTitleListings(types, source_types, source_ids, genre_ids);
+/**
+ * Queries external API(s) and returns a list of titles matching the query params
+ * @param {String} types The types of titles to query with (movie, tv, etc)
+ * @param {String} source_types The source types to query with (buy, sub, etc)
+ * @param {String} source_ids The IDs of sources to query with
+ * @param {String} genre_ids The IDs for the genres to query with
+ * @param {String} sort_by How to sort the results (popularity, relevance, etc)
+ * @returns A list of basic data for movies matching the query params
+ */
+export const getListTitlesResults = async (types, source_types, source_ids, genre_ids, sort_by) => {
+    return await watchmode.fetchTitleListings(types, source_types, source_ids, genre_ids, sort_by);
 }
 
 /**
