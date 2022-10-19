780560bd7b2ade73c2701933b4ccb8294af15b2c
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2022-10-18 23:27:28 -0400

    Fixed getTitleListings func and added docstrings

--- movi/server/actions/titleActions.js
+++ movi/server/actions/titleActions.js
@@ -4,26 +4,38 @@ import exApiActions from "../externalApi/actions.js";
 import TitleSource from "../models/titleSource.js";
 import Title from "../models/title.js";
 
-// GET request logic - (R)ead in CRUD
-// export const getTitles = async (req,res) => {
-//     res.send('This is the GET response for the localhost:5000/titles route...');
-// }
-
+/**
+ * Fetches and returns the title with the passed ID, or null if ID is invalid
+ * @param {Number} id The ID of the title to fetch
+ * @returns The title object with the matching ID, along with list of sources
+ */
 async function fetchTitleById(id) {
+    // check database for a matching title ID
     let title = await Title.findOne({ watchmodeId: id });
 
+    // If one wasn't found, populate from external APIs
     if (title == null) { 
         console.log(`Title with id ${id} not found in database. Fetching from third-party API...`);
         title = await exApiActions.populateTitleById(id);
     }
+    // If the ID still wasn't found, return null
     if (title == null) return null;
 
+    // Append the sources list to the returned object
     let sources = await TitleSource.where("title").equals(title._id);
     title = title.toObject();
     title.sources = sources;
     return title;
 }
 
+/**
+ * Handles an HTTP get request for a title with the path-variable-parameterized ID.
+ * Example query: localhost:5000/title/:id, where :id is the title ID 
+ *                localhost:5000/title/37764
+ * @param req HTTP request
+ * @param res HTTP response
+ * @returns JSON containing the object with sources or null for invalid request
+ */
 export const getTitleById = async (req, res) => {
     let { id } = req.params;
     id = parseInt(id);
@@ -32,8 +44,27 @@ export const getTitleById = async (req, res) => {
     res.json(title);
 }
 
+/**
+ * Handles an HTTP get request for titles whose name contains the 
+ * query-parameterized substring.
+ * Example query: localhost:5000/title/search/?search_query=harry%20potter
+ * 
+ * Additional query params:
+ *      verbose: should be true or false
+ *      WARNING: verbose will populate the database for each title in the
+ *               results and return a populated list. Functions similarly
+ *               to if you did a separate ID query for each result.
+ *               USE WITH CAUTION, this eats up API credits like crazy.
+ * 
+ * @param req HTTP request
+ * @param res HTTP response: JSON containing the list query results.
+ */
 export const getAutoCompleteSearchResults = async (req,res) => {
     let { search_query, verbose } = req.query;
+
+    // ... check remaining external API usage
+    // If out of credit, search internally...
+    // otherwise do the following:
 
     let results = await exApiActions.searchTitlesByPartialString(search_query);
 
@@ -52,20 +83,42 @@ export const getAutoCompleteSearchResults = async (req,res) => {
     res.json(titles);
 }
 
+/**
+ * Returns a list of titles filtered by the provided query parameters. Does not
+ * support a parameter to search by title string/substring (blame Watchmode).
+ * 
+ * The following query parameters are supported:
+ * - types (optional)
+ *      Filter result to only include certain types of titles. Pass a single 
+ *      type or pass multiple types comma delimited. Possible values: movie, 
+ *      tv_series, tv_special, tv_miniseries, short_film
+ * 
+ * - source_types (optional)
+ *      Filter results to only include titles that are available on a specific
+ *      type(s) of source (such a subscription, or TV Everywhere channel apps,
+ *      etc). By default all are selected, pass one or multiple (comma 
+ *      delimited) of these values: sub, rent, buy, free, tve
+ * 
+ * - source_ids (optional)
+ *      Pass an individual ID for a source (returned from the
+ *      /config/providers/ endpoint) to filter the results to titles available
+ *      on that source. Pass multiple values comma separated to return titles
+ *      available on one of the sources you pass in.
+ * 
+ * - genres (optional)
+ *      Filter results to only include certain genre(s). Pass in a single genre
+ *      id (which you would get from the /config/genres/ endpoint, or multiple 
+ *      comma separated.
+ * 
+ * - sort_by (optional)
+ *      Sort order of results, possible values: relevance_desc, relevance_asc, 
+ *      popularity_desc, popularity_asc, release_date_desc, release_date_asc, 
+ *      title_desc, title_asc. Default value is: relevance_desc.
+ * @param req The HTTP request
+ * @param res The HTTP response: JSON containing the list of query results.
+ */
 export const getTitleListings = async (req, res) => {
-    let { types, source_types, source_ids, genre_ids } = req.query;
-    let results = await exApiActions.getListTitlesResults(types, source_types, source_ids, genre_ids)
-    
+    let { types, source_types, source_ids, genre_ids, sort_by } = req.query;
+    let results = await exApiActions.getListTitlesResults(types, source_types, source_ids, genre_ids, sort_by);
+    res.json(results);
 }
-
-// export const postTitles = async (req, res) => {
-//     res.send('This is the POST response for the localhost:5000/titles route...');
-// }
-
-// export const putTitles = async (req, res) => {
-//     res.send('This is the PUT response for the localhost:5000/titles route...');
-// }
-
-// export const deleteTitles = async (req, res) => {
-//     res.send('This is the DELETE response for the localhost:5000/titles route...');
-// }
