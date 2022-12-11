a6be69c3fecc351f644deaf22c10f7f462a21188
jjm8759 <89434744+jjm8759@users.noreply.github.com>
2022-12-10 20:26:51 -0500

    Created register page based off login, issues with error handling

--- movi/client/src/App.jsx
+++ movi/client/src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css";
-import Login from "./Pages/Login/Login";
+import './App.css';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/login" element={<Login/>} />
+        <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/register' element={<Register/>} />
       </Routes>
     </Router>
   );
--- movi/client/src/Pages/Register/Register.jsx
+++ movi/client/src/Pages/Register/Register.jsx
@@ -1,62 +1,67 @@
-import useFetchData from "./authCalls";
-import React from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import { Form, Button } from 'semantic-ui-react';
-const Register = () => {
-    const {
-        data,
-        loading,
-    } = useFetchData();
+import React, { useState } from 'react';
+import api from '../../apiCall.js';
+import './Register.scss';
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-
-    const onSubmit = (data) => {
-        axios.post("http://localhost:5000/user/signup",{
-            firstName: data.firstName,
-            lastName: data.lastName,
-            email: data.email,
-            password:data.password
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      api.put('/user/signup', {
+        email,
+        password,
+        firstName,
+        lastName,
+      })
+        .then((res) => {
+          setUserData(res.data);
+        })
+        .catch((err) => {
+          setError(err.message);
         });
+      setError(null);
     }
+  }
+  return (
+      <div className='main'>
+        <h1 className='header'>Register To MOVI</h1>
+        
+        <form className='form' onSubmit={handleSubmit}>
+          {error && <p>{error}</p>}
+          <input class="form__input"
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input class="form__input"
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input class="form__input"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input class="form__input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className='button' type="submit">Register</button>
+          {userData && <p>Registered successfully</p>}
+        </form>
+      </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>Register To MOVI</h1>
-            {loading && <div>Loading</div>}
-            {!loading && (
-                <div>
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-
-                        <Form.Field>
-                            <label>First Name:</label>
-                            <input placeholder='First Name' type="text" {...register("firstName", { required: true})}/>
-                            {errors.firstName && <span>Please input a First Name</span>}
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Last Name:</label>
-                            <input placeholder='Last Name' type="text" {...register("lastName", { required: true})}/>
-                            {errors.lastName && <span>Please input a Last Name</span>}
-                        </Form.Field>
-
-                        <Form.Field>
-                            <label>Email:</label>
-                            <input placeholder='Email' type="text" {...register("Email", { required: true })}/>
-                            {errors.email && <span>Please input an email</span>}
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Password:</label>
-                            <input placeholder='Password' type="text" {...register("password", { required: true, maxLength: 18 })}/>
-                            {errors.password && <span>Please input a password</span>}
-                        </Form.Field>
-                        <button type="button">Log In</button>
-                    </Form>
-                    <Button type="submit">Register</Button>
-                </div>
-            )}
-
-        </div>
-    )
-}
-
-export default Register;
+export default Login;
